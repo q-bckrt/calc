6065f93bab79d9b9a944cb3cd9dad32050fce6b8
q-bckrt <pom.schc@gmail.com>
2023-03-24 03:52:23 +0100

    made the event handler for key inputs a separate function instead of an arrow callback for readability

--- calc.js
+++ calc.js
@@ -1,8 +1,15 @@
 const currValue = document.getElementById("curr-value");
 const btnList = document.getElementsByClassName("btn");
 
-[...btnList].forEach(e => {
-    e.addEventListener('click', e => {
+let numbersDisplayed = 0;
+
+function manageInputs(e) {
+    if (numbersDisplayed <= 12) {
         currValue.textContent += e.target.dataset.key;
-    })
-})
+        numbersDisplayed++;
+    }
+}
+
+[...btnList].forEach(e => {
+    e.addEventListener('click', manageInputs);
+});
