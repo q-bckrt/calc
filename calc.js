2a9051904d3e3fbeac32928b4dcda3adeb64b845
q-bckrt <pom.schc@gmail.com>
2023-03-25 23:31:24 +0100

    redone the logic slightly

--- calc.js
+++ calc.js
@@ -1,11 +1,10 @@
 const currDisplayedValue = document.getElementById("curr-value");
 const btnList = document.getElementsByClassName("btn");
 
-let currValue = 0;
+let lValue = null;
+let rValue = null;
 let numbersDisplayed = 0;
 
-let numberA = null;
-let numberB = null;
 let operator = '';
 
 function assignInputs(input) {
@@ -14,71 +13,71 @@ function assignInputs(input) {
         case '-':
         case '/':
         case '*':
-            if (numberA != null && numberB != null) {
-                operate();
-            } else {
-                currDisplayedValue.textContent = '';
-                numbersDisplayed = 0;
-            } 
-            operator = input;
-            break;
-        case '=':
-            if (numberA != null && numberB != null) {
+            if (lValue != null && rValue != null) {
+                console.log("operate from operator");
                 operate();
             }
+            lValue = rValue;
+            rValue = null;
+            operator = input;
+            currDisplayedValue.textContent = '';
+            numbersDisplayed = 0;
+            
+            break;
+        case '=':
+            operate();
             break;
         case '.':
             break;
         case 'clr':
+            clear();
             break;
         case 'chs':
             break;
         default:
             if (numbersDisplayed <= 12) {
                 currDisplayedValue.textContent += input;
-                if (operator == '') {
-                    numberA = parseFloat(currDisplayedValue.textContent);
-                } else {
-                    numberB = parseFloat(currDisplayedValue.textContent);
-                }
+                rValue = parseFloat(currDisplayedValue.textContent);
                 numbersDisplayed++;
             }
     } 
 }
 
 function operate() {
-    let result = 0;
 
     switch (operator) {
         case '+':
-            result = numberA + numberB;
+            rValue = lValue + rValue;
             break;
         case '-':
-            result = numberA - numberB;
+            rValue = lValue - rValue;
             break;
         case '/':
-            result = numberA / numberB;
+            rValue = lValue / rValue;
             break;
         case '*':
-            result = numberA * numberB;
+            rValue = lValue * rValue;
             break;
         default:
-            result = 0;
             break;
     }
-    numberB = result;
-    numberA = numberB;
-    numberB = null;
+    lValue = null;
+    currDisplayedValue.textContent = rValue.toString();
     operator = '';
-    currDisplayedValue.textContent = result.toString();
+}
+
+function clear() {
+    lValue = null;
+    rValue = null;
+    operator = '';
+    numbersDisplayed = 0;
+    currDisplayedValue.textContent = '';
 }
 
 function manageInputs(e) {
     assignInputs(e.target.dataset.key);
 
-    console.log("nA: ", numberA);
-    console.log("op: ", operator);
-    console.log("nB: ", numberB);
+    console.table({lValue, operator, rValue});
     console.log("--------------");
 
 }
